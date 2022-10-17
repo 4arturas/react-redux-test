51b04ea4d54c7da52dc47c526ed7722bac35d47d
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-10-17 07:38:32 +0300

    Some fixes

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,18 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import {Action, combineReducers, configureStore, ThunkAction} from '@reduxjs/toolkit'
 import type { PreloadedState } from '@reduxjs/toolkit'
 
 import userReducer from '../features/users/usersSlice'
+import counterReducer from '../features/counter/counterSlice';
 
 // Create the root reducer separately so we can extract the RootState type
 const rootReducer = combineReducers({
-  user: userReducer
+  user: userReducer,
+  counter: counterReducer,
 })
+
+export const store = configureStore({
+  reducer: rootReducer
+});
 
 export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
   return configureStore({
@@ -18,3 +24,9 @@ export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
 export type RootState = ReturnType<typeof rootReducer>
 export type AppStore = ReturnType<typeof setupStore>
 export type AppDispatch = AppStore['dispatch']
+export type AppThunk<ReturnType = void> = ThunkAction<
+    ReturnType,
+    RootState,
+    unknown,
+    Action<string>
+    >;
